ebb8b1da60ffd9ad7fd2613093f783c3d696c4a9
Aldair Beltran <aldairbeltran3@hotmail.com>
2020-06-02 19:35:32 -0500

    fking config

--- test/main.js
+++ test/main.js
@@ -9,20 +9,22 @@
 let fs = require("fs");
 let path = require("path");
 
-global.Config = {
-    servers: {
-        "testland":{
-            id: 'testland',
-            host: '0.0.0.0',
-            port: 8000,
-            rooms: ['mocha'],
-            name: "Edna Moda",
-            password: "Yourpasswordedna<3",
-            language: 'english',
-            initCmd: [],
-        }
-    },
-}
+require('../bot');
+
+global.Config = require('../config/config-example');
+
+Config.servers = {
+    "testland":{
+        id: 'testland',
+        host: '0.0.0.0',
+        port: 8000,
+        rooms: ['mocha'],
+        name: "Edna Moda",
+        password: "Yourpasswordedna<3",
+        language: 'english',
+        initCmd: [],
+    }
+};
 
 
 function testFolder(folder) {
