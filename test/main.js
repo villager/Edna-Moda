3de15af3c297389b50e33f52946917ccf69e4cba
Aldair Beltran <aldairbeltran3@hotmail.com>
2020-06-02 19:41:06 -0500

    a.a

--- test/main.js
+++ test/main.js
@@ -9,22 +9,37 @@
 let fs = require("fs");
 let path = require("path");
 
+before("Init", () => {
+	let Config;
+	try {
+		require.resolve('../config/config');
+	} catch (err) {
+		if (err.code !== 'MODULE_NOT_FOUND' && err.code !== 'ENOENT') throw err; // Should never happen
+
+		console.log("config.js doesn't exist - creating one with default settings...");
+		fs.writeFileSync(
+			path.resolve(__dirname, '../config/config.js'),
+			fs.readFileSync(path.resolve(__dirname, '../config/config-example.js'))
+		);
+	} finally {
+		Config = require('../config/config');
+    }
+    Config.servers = {
+        "testland":{
+            id: 'testland',
+            host: '0.0.0.0',
+            port: 8000,
+            rooms: ['mocha'],
+            name: "Edna Moda",
+            password: "Yourpasswordedna<3",
+            language: 'english',
+            initCmd: [],
+        }
+    };
+});
+
 require('../bot');
 
-global.Config = require('../config/config-example');
-
-Config.servers = {
-    "testland":{
-        id: 'testland',
-        host: '0.0.0.0',
-        port: 8000,
-        rooms: ['mocha'],
-        name: "Edna Moda",
-        password: "Yourpasswordedna<3",
-        language: 'english',
-        initCmd: [],
-    }
-};
 
 
 function testFolder(folder) {
